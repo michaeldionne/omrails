229fdd511e9e6077c7eeff0249c8a2c443905c50
michaeldionne <dionnemj1@gmail.com>
2013-07-16 20:33:20 -0700

    Attr_accessible changes

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,6 @@
 class Pin < ActiveRecord::Base
 
-attr_accessible :description, :image
+  attr_accessible :description, :image
 
   validates :description, presence: true
   validates :user_id, presence: true
