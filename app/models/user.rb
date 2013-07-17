22b7315ac1b21ad997f51868d396703c0d938ed2
michaeldionne <dionnemj1@gmail.com>
2013-07-16 22:22:48 -0700

    Paperclip temporary fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,11 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, #:recoverable,
          :rememberable, :trackable, :validatable
   validates_presence_of :name   
   has_many :pins
+
+   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+   
 end
