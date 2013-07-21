e0786439f6f64cbc8ece6d6551c31b0378e97925
michaeldionne <dionnemj1@gmail.com>
2013-07-21 09:33:00 -0700

    Fixed commit error.

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,19 +1,11 @@
 class Pin < ActiveRecord::Base
   attr_accessible :description, :image
 
-<<<<<<< HEAD
   validates :description, presence: true
   validates :user_id, presence: true
   validates_attachment :image, presence: true,
                             content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
                             size: { less_than: 5.megabytes }
-=======
-#  validates :description, presence: true
-#  validates :user_id, presence: true
-#  validates_attachment :image, #presence: true,
-#                            content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
-#                            size: { less_than: 5.megabytes }
->>>>>>> 05f7326e4be0a598ecd3f925c42fc5bd6ff8f6d0
   belongs_to :user
   has_attached_file :image, styles: { medium: "320x240>"}
 end
